c671d83e176b185bf42f6f6ad957677d9518a96f
Vincenzo Ampolo <vampolo@riverbed.com>
2013-08-07 12:26:38 -0700

    Small api management refactoring in order to reuse code

--- rvbd/shark/_api4.py
+++ rvbd/shark/_api4.py
@@ -6,7 +6,7 @@
 # This software is distributed "AS IS" as set forth in the License.
 
 
-from rvbd.shark._api_helpers import APIGroup, APITimestampFormat
+from rvbd.shark._api_helpers import APIGroup, APITimestampFormat, API
 import urllib
 
 class API4Group(APIGroup):
@@ -743,7 +743,9 @@ class Misc(API4Group):
         resp = self.shark.conn.request(self.uri_prefix + "/ping", method, body=data, extra_headers=headers)
         return resp.read()
     
-class API4_0(object):
+class API4_0(API):
+    version = '4.0'
+    common_version = '1.0'
     def __init__(self, shark):
         self.shark = shark
         self.common = Common("/api/common/1.0", self.shark)
--- rvbd/shark/_api_helpers.py
+++ rvbd/shark/_api_helpers.py
@@ -7,18 +7,17 @@
 
 
 
-class SharkAPIVersions:
+class SharkAPIVersions(object):
+    CURRENT = "5.0"
+    LEGACY = ["4.0"]
 
-    CURRENT = "4.0"
-    LEGACY = ["3.2", "3.1", "3.0"]
-
-class APIGroup:
+class APIGroup(object):
     '''Simple wrapper class for a group of related API functions'''
     def __init__(self, uri_prefix, shark):
         self.uri_prefix = uri_prefix
         self.shark = shark
 
-class APITimestampFormat:
+class APITimestampFormat(object):
     '''Encapsulation of the various options supported for absolute
     timestamp encoding.'''
     NANOSECOND             = "ns number"
@@ -36,3 +35,27 @@ class APITimestampFormat:
     SECOND_MILLISECOND     = "s.ms number"
     SECOND_MILLISECOND_STR = "s.ms string"
 
+class API(object):
+    version = NotImplementedError()
+    common_version = NotImplementedError()
+    def __init__(self, shark):
+        self.shark = shark
+        self.common = Common("/api/common/"+self.common_version, self.shark)
+        self.auth = Auth("/api/shark/"+self.version, self.shark)
+        self.settings = Settings("/api/shark/"+self.version, self.shark)
+        self.interfaces = Interfaces("/api/shark/"+self.version, self.shark)
+        self.jobs = Jobs("/api/shark/"+self.version, self.shark)
+        self.clips = Clips("/api/shark/"+self.version, self.shark)
+        self.fs = Files("/api/shark/"+self.version, self.shark)
+        self.licenses = Licenses("/api/shark/"+self.version, self.shark)
+        self.certificates = Certificates("/api/shark/"+self.version, self.shark)
+        self.system = System("/api/shark/"+self.version, self.shark)
+        self.view = Views("/api/shark/"+self.version, self.shark)
+        self.stats = Stats("/api/shark/"+self.version, self.shark)
+        self.info = Info('/api/shark/'+self.version+'/info', self.shark)
+        self.users = Users('/api/shark/'+self.version, self.shark)
+        self.groups = Users('/api/shark/'+self.version, self.shark)
+
+        # For the misc handlers just make them methods of the api class itself
+        m = Misc('/api/shark/'+self.version, self.shark)
+        self.ping = m.ping
