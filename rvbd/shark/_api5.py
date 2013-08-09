6a99ddc1a3fee131b5f3be97e1615ffa2891d6df
Vincenzo Ampolo <vampolo@riverbed.com>
2013-08-09 15:36:10 -0700

    Added copyright notice to new files

--- rvbd/shark/_api5.py
+++ rvbd/shark/_api5.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the 
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").  
+# This software is distributed "AS IS" as set forth in the License.
+
 from _api4 import API4_0
 
 class API5_0(API4_0):
--- rvbd/shark/_source5.py
+++ rvbd/shark/_source5.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2013 Riverbed Technology, Inc.
+#
+# This software is licensed under the terms and conditions of the 
+# MIT License set forth at:
+#   https://github.com/riverbed/flyscript/blob/master/LICENSE ("License").  
+# This software is distributed "AS IS" as set forth in the License.
+
 from rvbd.shark import _source4 as s4
 
 class Job5(s4.Job4):
